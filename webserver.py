dee21dedac839059a7b11cf7bc8535885e0eaef3
RaymondW9 <60833199+RaymondW9@users.noreply.github.com>
2020-04-10 16:12:25 -0400

    Added option for user to set duration

--- webserver.py
+++ webserver.py
@@ -5,21 +5,30 @@ from random import randint
 app = Flask(__name__)
 app.secret_key = "a, b, c where a^n + b^n = c^n for n > 2"
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def start():
     session['points'] = 0
     session['num1'] = randint(0, 100)
     session['num2'] = randint(0, 100)
     session['started'] = 0
-    return render_template('start.html', url=url_for('quiz'))
+    session['time'] = 60
+    if request.method == 'POST':
+        try:
+            session['time'] = int(request.form['time'])
+            if session['time'] < 10 or session['time'] > 300:
+                session['time'] = 60
+        except:
+            pass
+    return render_template('start.html', url=url_for('quiz'), time=session['time'])
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
-    #if quiz just started, record the end-time. Otherwise, prompt the user to return to the start page
+    
+    #if the challenge was started, record the end-time. Otherwise, prompt the user to return to the start page
     try:
         if session['started'] == 0:
             session['started'] = 1
-            session['endtime'] = datetime.datetime.now() + datetime.timedelta(seconds=45)
+            session['endtime'] = datetime.datetime.now() + datetime.timedelta(seconds=session['time'])
     except:
         return render_template('results.html', url=url_for('start'), score=-1)
     
@@ -29,9 +38,8 @@ def quiz():
     
     #Check the user's answer
     if request.method == 'POST':
-        userAns = request.form['userAns']
         try:
-            ans = int(userAns)
+            ans = int(request.form['userAns'])
             if ans == session['num1'] + session['num2']:
                 session['points'] = session['points'] + 1
         except:
