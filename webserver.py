959a03428a0ebd74a760014b2886149c4b69918d
RaymondW9 <60833199+RaymondW9@users.noreply.github.com>
2020-04-11 16:14:50 -0400

    Added option for user to set mode

--- webserver.py
+++ webserver.py
@@ -12,14 +12,29 @@ def start():
     session['num2'] = randint(0, 100)
     session['started'] = 0
     session['time'] = 60
+    session['mode'] = '+'
     if request.method == 'POST':
+        #get the user-inputted duration
         try:
             session['time'] = int(request.form['time'])
             if session['time'] < 10 or session['time'] > 300:
                 session['time'] = 60
         except:
             pass
-    return render_template('start.html', url=url_for('quiz'), time=session['time'])
+        #get the user-inputted mode
+        try:
+            session['mode'] = request.form['mode']
+        except:
+            pass
+    if session['mode'] == '+':
+        mode = 'addition'
+    if session['mode'] == '-':
+        mode = 'subtraction'
+    if session['mode'] == '*':
+        mode = 'multiplication'
+    if session['mode'] == '/':
+        mode = 'division'
+    return render_template('start.html', url=url_for('quiz'), time=session['time'], mode=mode)
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
@@ -40,12 +55,20 @@ def quiz():
     if request.method == 'POST':
         try:
             ans = int(request.form['userAns'])
-            if ans == session['num1'] + session['num2']:
+            if session['mode'] == '+':
+                correctAns = session['num1'] + session['num2']
+            if session['mode'] == '-':
+                correctAns = session['num1'] - session['num2']
+            if session['mode'] == '*':
+                correctAns = session['num1'] * session['num2']
+            if session['mode'] == '/':
+                correctAns = session['num1'] / session['num2']
+            if ans == correctAns:
                 session['points'] = session['points'] + 1
         except:
             pass
     session['num1'] = randint(0, 100)
     session['num2'] = randint(0, 100)
-    return render_template('quiz.html', num1=session['num1'], num2=session['num2'], points=session['points'])
+    return render_template('quiz.html', num1=session['num1'], num2=session['num2'], points=session['points'], mode=session['mode'])
 
 app.run(host='0.0.0.0', port=5000, debug=True)
