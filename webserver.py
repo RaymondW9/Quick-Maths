595ed57c3ef19801669bc31293dee1b686d80283
RaymondW9 <60833199+RaymondW9@users.noreply.github.com>
2020-04-17 17:05:55 -0400

    Added CSS

--- webserver.py
+++ webserver.py
@@ -86,6 +86,6 @@ def quiz():
             session['num2'] = 1
         #avoid decimal answers
         session['num1'] = randint(session['min'], session['max']) * session['num2']
-    return render_template('quiz.html', num1=session['num1'], num2=session['num2'], points=session['points'], mode=session['mode'])
+    return render_template('quiz.html', num1=session['num1'], num2=session['num2'], points=session['points'], mode=session['mode'], url=url_for('start'))
 
 app.run(host='0.0.0.0', port=5000, debug=True)
